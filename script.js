a27a029380e52ac7af2e7755ce9f1f4b229020c2
Aisenem02 <aisenem.ruslanova.kk@gmail.com>
2022-02-25 13:56:38 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const btn = document.getElementById('btn')
+const quoteDisplay = document.getElementById("quoteDisplay")
+
+let quotes = [
+    ["Stay Hungry. Stay Foolish.", "Steve Jobs"],
+    ["Good Artists Copy, Great Artists Steal.", "Pablo Picasso"],
+    ["Argue with idiots, and you become an idiot.", "Paul Graham"],
+    ["Be yourself; everyone else is already taken.", "Oscar Wilde"],
+    ["Simplicity is the ultimate sophistication.", "Leonardo Da Vinci"]
+  ]
+
+  const randomQuotes = () =>{
+    let randomNumber = Math.floor(Math.random() * quotes.length)
+    quoteDisplay.innerHTML=quotes[randomNumber]
+  }
+
+  btn.onclick=()=>{
+     randomQuotes()
+  }
+
+  
